efa88b6929187b813642467696c1b072e76470d7
dd2558 <dd2558@naver.com>
2024-06-19 00:21:46 +0900

    function,변수값정의

--- test.py
+++ test.py
@@ -1,1 +1,31 @@
-print("hello world")
+def say_hello():
+  print("Hi guys")
+  
+def say_bye():
+  print("Bye guys")
+  
+def my_name():
+  print("My name is 동원")  
+  
+def double_func():
+  print("One")
+  print("Two")  
+
+a=2
+b=5
+
+def cal(a,b):
+  print(a+b)
+  
+def cal2(a,b):
+  a=5
+  b=10
+  print(a+b)  
+  
+
+say_hello()
+say_bye()
+my_name()
+double_func()
+cal(a,b)
+cal2(a,b)
